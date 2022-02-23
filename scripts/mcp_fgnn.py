22cfe85a3c5c16a9c6e0b4365ae123a93d873522
MauTrib <amaury.triboulin@gmail.com>
2022-02-23 18:53:47 +0100

    stopping wandb logging

--- scripts/mcp_fgnn.py
+++ scripts/mcp_fgnn.py
@@ -4,6 +4,7 @@ sys.path.append(os.getcwd())
 from toolbox.planner import Task, Planner
 from commander import get_config, train, test
 from pytorch_lightning import seed_everything
+import wandb
 
 seed_everything(5344)
 
@@ -20,16 +21,15 @@ tasks = [Task('clique_size', value) for value in l_clique_sizes]
 planner = Planner(planner_path)
 planner.add_tasks(tasks)
 
-BASE_CONFIG = get_config(config_path)
-
 def step(planner):
     task = planner.next_task()
     print(f"Task: {task}")
-    config = deepcopy(BASE_CONFIG)
+    config = get_config(config_path)
     config['data']['train']['problems']['mcp']['clique_size'] = task.value
     config['data']['test']['problems']['mcp']['clique_size'] = task.value
     trainer = train(config)
     test(trainer, config)
+    wandb.finish()
     planner.add_entry({task.column_name:task.value, "done":True})
 
 
