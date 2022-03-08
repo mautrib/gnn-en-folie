b8eb04be54426d558f4a40520e0aaca4f1289656
MauTrib <amaury.triboulin@gmail.com>
2022-03-08 17:00:40 +0100

    now saving advancements in a log file for the sweeps, temporary workaround for wandb leaking files when closing...

--- scripts/fgnn/mcp_grid.py
+++ scripts/fgnn/mcp_grid.py
@@ -3,7 +3,7 @@ import sys, os
 sys.path.append(os.getcwd())
 import tqdm
 from data import get_test_dataset
-from toolbox.planner import DataHandler
+from toolbox.planner import DataHandler, Planner
 from commander import get_config, load_model, setup_metric, setup_trainer
 import wandb
 wb_api = wandb.Api()
@@ -17,9 +17,11 @@ MODEL = 'fgnn'
 WANDB_ENTITY = 'mautrib'
 WANDB_PROJECT = f"repr_{PROBLEM}"
 
+#VALUES_DEPENDING ON ABOVE
 MODELS_DIR = f'observers/repr_{PROBLEM}/'
 BASE_PATH = f'scripts/{MODEL}/'
 DATA_FILE = os.path.join(BASE_PATH, f'planner_files/recap_{PROBLEM}.csv')
+ADVANCE_LOG_FILE = os.path.join(BASE_PATH, f'planner_files/sweep_log_{PROBLEM}.csv')
 CONFIG_FILE_NAME = f'{PROBLEM}_{MODEL}.yaml'
 CONFIG_FILE = os.path.join(BASE_PATH, CONFIG_FILE_NAME)
 BASE_CONFIG = get_config(CONFIG_FILE)
@@ -29,6 +31,7 @@ ERASE_DATASETS = True
 VALUES = range(5,21)
 
 DH = DataHandler(DATA_FILE)
+planner = Planner(ADVANCE_LOG_FILE)
 
 def get_exp_name_from_ckpt_path(ckpt_path):
     name,_ = ckpt_path.split('/checkpoints/')
@@ -64,18 +67,21 @@ def step(path_to_chkpt):
     setup_metric(pl_model, config)
     progress_bar = tqdm.tqdm(VALUES)
     for value in progress_bar:
-        progress_bar.set_description(f'Value {value}.')
-        test_dataset = prepare_dataset(config, value)
-        trainer = setup_trainer(config, pl_model, watch=False)
-        wandblogger = trainer.logger.experiment
-        wandblogger.config['train_value'] = train_value
-        trainer.test(pl_model, test_dataloaders=test_dataset)
-        wandb.finish()
-        logged_metrics = trainer.logged_metrics
-        clean_metrics = handle_data(logged_metrics)
-        clean_metrics[VALUE_NAME] = value
-        clean_metrics['train_value'] = train_value
-        DH.add_entry(clean_metrics)
+        planner_line = {'train_value': train_value, 'test_value': value, 'done': True}
+        if not planner.line_exists(planner_line):
+            progress_bar.set_description(f'Value {value}.')
+            test_dataset = prepare_dataset(config, value)
+            trainer = setup_trainer(config, pl_model, watch=False)
+            wandblogger = trainer.logger.experiment
+            wandblogger.config['train_value'] = train_value
+            trainer.test(pl_model, test_dataloaders=test_dataset)
+            wandb.finish()
+            logged_metrics = trainer.logged_metrics
+            clean_metrics = handle_data(logged_metrics)
+            clean_metrics[VALUE_NAME] = value
+            clean_metrics['train_value'] = train_value
+            DH.add_entry(clean_metrics)
+            planner.add_entry(planner_line)
 
 
 to_walk = os.walk(MODELS_DIR)
