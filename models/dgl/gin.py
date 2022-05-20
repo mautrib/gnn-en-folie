011896d39616b446743cec901ef9883d16bc97f0
MauTrib <amaury.triboulin@gmail.com>
2022-05-20 14:15:32 +0200

    Added GINEdge

--- models/dgl/gin.py
+++ models/dgl/gin.py
@@ -193,4 +193,90 @@ class GINEdgeSimple(nn.Module):
         
         return g.edata['e']
     
-    
+class GINEdge(nn.Module):
+    """GIN Edge model inspired by : https://github.com/graphdeeplearning/benchmarking-gnns/blob/master/nets/TSP_edge_classification/gin_net.py"""
+    def __init__(self, n_layers, depth_of_mlp, in_features, hidden_features,
+                 n_classes, final_dropout=0., learn_eps=False, graph_pooling_type='sum',
+                 neighbor_pooling_type='sum'):
+        super(GINEdge, self).__init__()
+        self.n_layers = n_layers
+        self.learn_eps = learn_eps
+
+        self.embedding_h = nn.Linear(in_features, hidden_features)
+
+        # List of MLPs
+        self.ginlayers = torch.nn.ModuleList()
+        self.batch_norms = torch.nn.ModuleList()
+
+        for layer in range(self.n_layers - 1):
+            if layer == 0:
+                mlp = MLP(depth_of_mlp, in_features, hidden_features, hidden_features)
+            else:
+                mlp = MLP(depth_of_mlp, hidden_features, hidden_features, hidden_features)
+
+            self.ginlayers.append(
+                GINConv(ApplyNodeFunc(mlp), neighbor_pooling_type, 0, self.learn_eps))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_features))
+
+        # Linear function for graph poolings of output of each layer
+        # which maps the output of different layers into a prediction score
+        self.linears_prediction = torch.nn.ModuleList()
+
+        for layer in range(n_layers):
+            if layer == 0:
+                self.linears_prediction.append(
+                    nn.Linear(in_features, n_classes))
+            else:
+                self.linears_prediction.append(
+                    nn.Linear(hidden_features, n_classes))
+
+        self.drop = nn.Dropout(final_dropout)
+
+        if graph_pooling_type == 'sum':
+            self.pool = SumPooling()
+        elif graph_pooling_type == 'mean':
+            self.pool = AvgPooling()
+        elif graph_pooling_type == 'max':
+            self.pool = MaxPooling()
+        else:
+            raise NotImplementedError
+
+        # Non-linear function for output of each layer
+        # which maps the output of different layers into a prediction score
+        self.prediction = torch.nn.ModuleList()
+
+        for layer in range(self.n_layers + 1):
+            self.prediction.append(
+                nn.Sequential(
+                    nn.Linear(2*hidden_features, hidden_features),
+                    nn.ReLU(),
+                    nn.Linear(hidden_features, n_classes)
+                )
+            )
+
+    def forward(self, g, h, e):
+        
+        def _edge_feat(edges):
+            e = torch.cat([edges.src['h'], edges.dst['h']], dim=1)
+            return {'e': e}
+        
+        h = self.embedding_h(h.float())
+        g.ndata['h'] = h
+        g.apply_edges(_edge_feat)
+        
+        # list of hidden representation at each layer (including input)
+        hidden_rep = [g.edata['e']]
+        
+        for i in range(self.n_layers):
+            h = self.ginlayers[i](g, h)
+            h = self.batch_norms[i](h)
+            h = F.relu(h)
+            g.ndata['h'] = h
+            g.apply_edges(_edge_feat)
+            hidden_rep.append(g.edata['e'])
+
+        score_over_layer = 0
+        for i, e in enumerate(hidden_rep):
+            score_over_layer += self.prediction[i](e)
+        
+        return score_over_layer
