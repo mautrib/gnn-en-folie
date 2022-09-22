3df52e48755b4e99ed7718cfcc385e522f53dbc0
MauTrib <amaury.triboulin@gmail.com>
2022-09-22 13:46:15 +0200

    Added MCP_solver with file I/O + now the pmc library can be in root of project

--- toolbox/searches/mcp/mcp_solver.py
+++ toolbox/searches/mcp/mcp_solver.py
@@ -4,13 +4,32 @@ import time
 import torch
 import numpy as np
 import random
-import toolbox.utils as utils
+import ctypes
+try:
+    import toolbox.utils as utils
+except ModuleNotFoundError:
+    import sys
+    sys.path.append(sys.path[0] + '/../../..')
+    import toolbox.utils as utils 
 
 from string import ascii_letters,digits
 NAME_CHARS = ascii_letters+digits
 
 from numpy.ctypeslib import ndpointer
 import ctypes
+
+def load_pmc():
+    LIB_NAME = "libpmc.so"
+    SEARCH_DIRS = [LIB_NAME, os.path.join(os.getcwd(), LIB_NAME), os.path.join(os.getcwd(),"../../..", LIB_NAME)]
+    lib=None
+    for search_dir in SEARCH_DIRS:
+        #print(f"Searching in {search_dir}")
+        try:
+            lib = ctypes.cdll.LoadLibrary(search_dir)
+            break
+        except OSError:
+            pass
+    return lib
 
 def pmc(ei,ej,nnodes,nnedges): #ei, ej is edge list whose index starts from 0
     degrees = np.zeros(nnodes,dtype = np.int32)
@@ -27,7 +46,7 @@ def pmc(ei,ej,nnodes,nnedges): #ei, ej is edge list whose index starts from 0
     new_ej = np.array(new_ej,dtype = np.int32)
     outsize = maxd
     output = np.zeros(maxd,dtype = np.int32)
-    lib = ctypes.cdll.LoadLibrary("libpmc.so")
+    lib = load_pmc()
     fun = lib.max_clique
     #call C function
     fun.restype = np.int32
@@ -67,8 +86,63 @@ class Thread_MCP_Solver(threading.Thread):
         self.solutions = [clique]
         self.done = True
 
+class Thread_MCP_File_Solver(threading.Thread):
+    def __init__(self, threadID, adj, name=''):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.adj = adj
+        if name=='':
+            name = ''.join(random.choice(NAME_CHARS) for _ in range(10))
+        self.name = name
+        self.fwname = name+'.mtx'
+        self.frname = name+'.mcps'
+        self.flname = name+'.log'
+        self.solutions = []
+        self.done=False
+
+    
+    def _write_adj(self):
+        src,dst = torch.where(self.adj!=0)
+        txt_to_write = "%%MatrixMarket matrix coordinate pattern symmetric\n"
+        txt_to_write+=f"{self.adj.shape[0]} {self.adj.shape[1]} {len(src)}\n"
+        txt_to_write+="\n".join([f"{s+1} {d+1}" for s,d in zip(src,dst) if s>d])
+        with open(self.fwname,'w') as f:
+            f.write(txt_to_write)
+    #def write_adj_pmc(fname,adj):
+    #    src,dst = torch.where(adj!=0)
+    #    txt_to_write = "%%MatrixMarket matrix coordinate pattern symmetric\n"
+    #    txt_to_write+=f"{adj.shape[0]} {adj.shape[1]} {len(src)}\n"
+    #    txt_to_write+="\n".join([f"{s+1} {d+1}" for s,d in zip(src,dst) if s>d])
+    #    with open(fname,'w') as f:
+    #        f.write(txt_to_write)
+
+    def _read_adj(self):
+        with open(self.flname,'r') as f:
+            data = f.readlines()
+        final_line = data[-1]
+        assert final_line[:16] == "Maximum clique: ", "Didn't understand last line..."
+        line = final_line[16:-2] #Remove the '\n'
+        clique = {int(elt) for elt in line.split(' ')}
+        cliques = [clique]
+        self.solutions = cliques
+    
+    def clear(self,erase_mode='all'):
+        if erase_mode=='all':
+            #os.remove(self.frname)
+            os.remove(self.fwname)
+            os.remove(self.flname)
+        elif erase_mode=='i':
+            os.remove(self.flname)
+            os.remove(self.fwname)
+
+    def run(self):
+        self._write_adj()
+        os.system(f"./pmc -f {self.fwname} >> {self.flname}")
+        self._read_adj()
+        self.done = True
+
 class MCP_Solver():
-    def __init__(self,adjs=None, max_threads=4, path='tmp_mcp/',erase_mode ='all'):
+    def __init__(self,adjs=None, max_threads=4, path='tmp_mcp/',erase_mode ='all', verbose=True, use_file=False):
         utils.check_dir(path)
         self.path = path
         if adjs is None:
@@ -79,6 +153,11 @@ class MCP_Solver():
         self.threads = [None for _ in range(self.max_threads)]
         self.solutions  = []
         self.erase_mode=erase_mode
+        self.verbose = verbose
+        self.thread_class = Thread_MCP_Solver
+        if load_pmc() is None:
+            if verbose: print("Using file version of solver (will be slower)")
+            self.thread_class = Thread_MCP_File_Solver
     
     @classmethod
     def from_data(adjs, **kwargs):
@@ -101,7 +180,7 @@ class MCP_Solver():
         for i,thread in enumerate(self.threads):
             if thread is not None and thread.done:
                 id = thread.threadID
-                print(f"Solution {id} on thread {i} is done.")
+                if self.verbose: print(f"Solution {id} on thread {i} is done.")
                 self.solutions[id] = thread.solutions
                 thread.clear(erase_mode=self.erase_mode)
                 self.threads[i] = None
@@ -135,7 +214,7 @@ class MCP_Solver():
             for thread_slot in range(self.max_threads):
                 if counter < bs and self.is_thread_available(thread_slot):
                     adj = adjs[counter]
-                    new_thread = Thread_MCP_Solver(counter,adj,name=os.path.join(self.path,f'tmp-mcp-{counter}-{exp_name}'))
+                    new_thread = self.thread_class(counter,adj,name=os.path.join(self.path,f'tmp-mcp-{counter}-{exp_name}'))
                     #print(f"Putting problem {counter} on thread {thread_slot}")
                     self.threads[thread_slot] = new_thread
                     new_thread.start()
@@ -149,7 +228,7 @@ if __name__=='__main__':
         adjs = torch.empty((bs,n,n)).uniform_()
         adjs = (adjs.transpose(-1,-2)+adjs)/2
         adjs = (adjs<(0.5)).to(int)
-        mcp_solver = MCP_Solver(adjs,max_threads)
+        mcp_solver = MCP_Solver(adjs,max_threads,use_file=True)
         mcp_solver.solve()
         clique_sols = mcp_solver.solutions
         return clique_sols
