2a9846ade0125eb6543309619fe9656ddc840ca9
MauTrib <amaury.triboulin@gmail.com>
2022-10-05 16:07:38 +0200

    Added possible ablation of RS and IN in RSFGNN

--- models/fgnn/fgnn.py
+++ models/fgnn/fgnn.py
@@ -95,11 +95,12 @@ class Simple_Edge_Embedding(nn.Module):
 
 
 class Res_Scaled_Model(nn.Module):
-    def __init__(self, original_features_num, num_blocks, in_features, out_features, depth_of_mlp, input_embed=True, **kwargs):
+    def __init__(self, original_features_num, num_blocks, in_features, out_features, depth_of_mlp, input_embed=True, ablation="", **kwargs):
         """
         take a batch of graphs (bs, n_vertices, n_vertices, in_features)
         and return a batch of graphs with new features
         graphs must have same size inside the batch
+         - ablation : "", "rs", "in", or "rsin" (resp : nothing changed, removing residuals, removing InstanceNorms, removing both Residuals and InstanceNorms)
         """
         super().__init__()
 
@@ -110,18 +111,25 @@ class Res_Scaled_Model(nn.Module):
         self.depth_of_mlp = depth_of_mlp
         self.embed = input_embed
 
+        if ablation is None: ablation=""
+        self.use_residuals = not "rs" in ablation
+        self.use_instancenorm = not "in" in ablation
+
         # First part - sequential mlp blocks
         if not self.embed:
             last_layer_features = self.original_features_num
         else:
             self.embedding = nn.Embedding(2,in_features)
             last_layer_features = self.in_features
+        
+        BlockClass = Scaled_Block if self.use_instancenorm else RegularBlock
+
         self.reg_blocks = nn.ModuleList()
         for i in range(self.num_blocks-1):
-            mlp_block = Scaled_Block(last_layer_features, in_features, self.depth_of_mlp, name=f'block_{i}')
+            mlp_block = BlockClass(last_layer_features, in_features, self.depth_of_mlp, name=f'block_{i}')
             self.reg_blocks.append(mlp_block)
             last_layer_features = in_features
-        mlp_block = Scaled_Block(in_features,in_features,depth_of_mlp,name=f'block_{self.num_blocks-1}')
+        mlp_block = BlockClass(in_features,in_features,depth_of_mlp,name=f'block_{self.num_blocks-1}')
         self.reg_blocks.append(mlp_block)
         self.last_mlp = nn.Conv2d(in_features,out_features,kernel_size=1, padding=0, bias=True)
 
@@ -137,7 +145,10 @@ class Res_Scaled_Model(nn.Module):
         #expects x.shape = (bs, n_features, n_vertices, n_vertices)
         #x.shape = (bs, n_features, n_vertices, _)
         for block in self.reg_blocks:
-            x = x + block(x)
+            if self.use_residuals:
+                x = x + block(x)
+            else:
+                x = block(x)
         # return (bs, n_vertices, n_vertices, n_features=in_features)
         x = self.last_mlp(x)
         return x.permute(0,2,3,1)
